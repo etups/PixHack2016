a245b45e5114025da304f25cbd50c4955f56f00d
raix852 <raix852@gmail.com>
2016-07-11 00:34:15 +0800

    [ADD] Add detect language detect_lang.py.

---
+++ jieba/detect_lang.py
@@ -0,0 +1,28 @@
+import os
+import langid
+import glob
+
+filter_lang = ['ja', 'ko']
+
+def main():
+
+    filter_dict = dict()
+    for filename in glob.glob('./*.txt'):
+        f = open(filename, 'r')
+        for line in f.readlines():
+            for word in line.split():
+                lang, conf = langid.classify(word)
+                if lang in filter_lang:
+                    if w in filter_dict:
+                        filter_dict[word] += 1
+                    else:
+                        filter_dict[word] = 0
+
+        f.close()
+
+    f = open("filter.txt", "w+")
+    for k, v in filter_dict.items():
+        f.write("%s %d" % (k, v))
+
+if __name__ == '__main__':
+    main()
